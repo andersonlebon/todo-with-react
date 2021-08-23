272bac69e0ec0794022be9c70d014678ac844824
Caleb Anderson <buyananderson@gmail.com>
2021-08-23 16:18:18 +0200

    Style tasks ul

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -4,25 +4,21 @@ import React from 'react';
 const Todo = (props) => {
   const { todo, onChange, onDelete } = props;
   return (
-    <li className="todo">
-      <div
-        className={todo.completed ? 'todo-control completed' : 'todo-control'}
+    <li className={todo.completed ? 'todo-control completed' : 'todo-control'}>
+      <input
+        onChange={onChange}
+        type="checkbox"
+        defaultChecked={todo.completed}
+        id={todo.id}
+      />
+      <label htmlFor={todo.id}>{todo.title}</label>
+      <button
+        onClick={() => onDelete(todo.id)}
+        type="button"
+        className="delele ml-auto"
       >
-        <input
-          onChange={onChange}
-          type="checkbox"
-          defaultChecked={todo.completed}
-          id={todo.id}
-        />
-        <label htmlFor={todo.id}>{todo.title}</label>
-        <button
-          onClick={() => onDelete(todo.id)}
-          type="button"
-          className="delele"
-        >
-          delete
-        </button>
-      </div>
+        delete
+      </button>
     </li>
   );
 };
