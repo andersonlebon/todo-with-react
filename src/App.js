ab15d4c124dcc007ff135dacaf62f9da1c35dfd6
Caleb Anderson <buyananderson@gmail.com>
2021-08-23 11:29:48 +0200

    Store the todo list in the store

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-function App() {
-  return <div className="App">hello</div>;
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todo: [{ id: 1, title: 'todo1', completed: false }],
+    };
+  }
+
+  render() {
+    const { todo } = this.state;
+    console.log(todo);
+    return <div>{todo[0].title}</div>;
+  }
 }
 
 export default App;
