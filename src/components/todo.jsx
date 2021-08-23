ba6a7f07a9e10c88834b90c248277e631ede5fdb
Caleb Anderson <buyananderson@gmail.com>
2021-08-23 17:55:00 +0200

    make the edit task functionnality

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -1,7 +1,16 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useState } from 'react';
+import Form from './addItem';
 
 const Todo = (props) => {
+  const [showEditer, setEditor] = useState(false);
+
+  const handelShow = () => {
+    // const id = parseInt(e.currentTarget.id, 10);
+    // const todo = todos.find((task) => task.id === id);
+    console.log(showEditer);
+    setEditor(true);
+  };
   const { todo, onChange, onDelete } = props;
   return (
     <li className={todo.completed ? 'todo-control completed' : 'todo-control'}>
@@ -11,7 +20,20 @@ const Todo = (props) => {
         defaultChecked={todo.completed}
         id={todo.id}
       />
-      <label htmlFor={todo.id}>{todo.title}</label>
+      <label
+        onDoubleClick={() => handelShow()}
+        aria-hidden="true"
+        htmlFor={todo.id}
+      >
+        {todo.title}
+      </label>
+      {/* <input
+        type="text"
+        className={showEditer ? 'd-flex' : 'd-none'}
+        placeholder="Modify"
+      /> */}
+
+      {showEditer ? <Form /> : null}
       <button
         onClick={() => onDelete(todo.id)}
         type="button"
