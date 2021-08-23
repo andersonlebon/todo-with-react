ce431e50c10abb589174194f575ffea15e0d4c50
Caleb Anderson <buyananderson@gmail.com>
2021-08-23 12:15:55 +0200

    Get the title in the todo

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
 
-
-
 const Todo = (props) => {
-    const {todos} = props;
-    return (
-      <li className="todo">
-        <div className="todo-control">
-          <input type="checkbox" id="check-b" />
-          <label htmlFor="check-b">{todo[0].title}</label>
-          <button type="button" className="delele">
-            delete
-          </button>
-        </div>
-      </li>
-    );
-}
- 
+  const { todo } = props;
+  return (
+    <li className="todo">
+      <div className="todo-control">
+        <input type="checkbox" id="check-b" />
+        <label htmlFor="check-b">{todo.title}</label>
+        <button type="button" className="delele">
+          delete
+        </button>
+      </div>
+    </li>
+  );
+};
+
 export default Todo;
