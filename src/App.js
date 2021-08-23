cfac5c209a5d0dae55766dc2d4164ae53cef750c
Caleb Anderson <buyananderson@gmail.com>
2021-08-23 11:57:28 +0200

    Add the input check box

--- src/App.js
+++ src/App.js
@@ -13,11 +13,20 @@ class App extends Component {
     return (
       <section>
         <div className="main-container">
-          <h1 className="todo">Todos</h1>
+          <h1 className="todo-heading">Todos</h1>
           <form className="form">
             <input type="text" placeholder="Add todo..." />
             <button type="submit">Submit</button>
           </form>
+
+          <ul className="todo-list">
+            <li className="todo">
+              <div className="todo-control">
+                <input type="checkbox" id="check-b" />
+                <label htmlFor="check-b">{todo[0].title}</label>
+              </div>
+            </li>
+          </ul>
         </div>
       </section>
     );
